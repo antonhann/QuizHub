e4e05428f17788ffcf9c974b1c224bbe601a2599
antonhann <antonha016@gmail.com>
2023-10-09 17:09:48 -0400

    fixed mongo server

--- server/index.js
+++ server/index.js
@@ -19,7 +19,7 @@ app.use(session({
 const mongoose = require("mongoose")
 // const user = require("./user")
 //insert pasword after 016:
-const url = "mongodb+srv://antonha016:ilovemongodb@quizhub.hnifsba.mongodb.net/?retryWrites=true&w=majority";
+const url = "mongodb+srv://antonha016:@quizhub.hnifsba.mongodb.net/?retryWrites=true&w=majority";
 mongoose.set("strictQuery",false)
 
 async function connect(){
